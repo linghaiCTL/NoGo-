408c404184d9573eedfd01e61bacb52aab0be205
linghaiCTL <118032097+linghaiCTL@users.noreply.github.com>
2022-11-14 18:41:43 +0800

    NoGo1.0.3版本

--- NoGo人类端.cpp
+++ NoGo人类端.cpp
@@ -1,17 +1,30 @@
-﻿#include <iostream>
+﻿//1.0.3版本更新日志：
+//更新日期：2022年11月14日
+//更新内容：1.优化了ai提升其对弈的胜率
+//          2.优化了程序结构提高运行效率
+//          3.更新了一部分操作内容从而优化操作体验
+//特别鸣谢：感谢吴雨洙对本次更新中算法思路的提供
+#include <iostream>
 #include <cstdio>
 #include <string>
 #include <cstring>
 #include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
 //#include "jsoncpp/json.h"
 using namespace std;
 bool oncheck[9][9]{ 0 };
-int value[9][9]{ 0 };
+int value[2][9][9]{ 0 };//价值函数，0为1，1为-1
 int qipan[9][9]{ 0 };
+int kexingyu[81][3]{ 0 };
 int fanqipan[9][9]{ 0 };
-bool ed = 0;
+bool av[2][9][9]{ 1 };//某位置是否可以走棋，0为1，1为-1
+bool kaiju = true;
+bool ifqihere[2][9][9];
+int qi[2][9][9];
 int p, q;
 char z;
+int ed = 0;
 int cc()
 {
 	char f;
@@ -34,7 +47,6 @@ bool checkqi(int a, int b)
 {
 	if (ifout(a, b))
 	{
-		cout << a << ' ' << b << endl;
 		return false;
 	}
 	else
@@ -87,46 +99,48 @@ bool ifqi(int aa, int bb, int mm)//此处为判断是否有气的函数合集
 	setf(mm);
 	return (checkqi(aa, bb));
 }
-void randomlyset(int m)//随机走棋
+void ifav()//判断可走棋位置
 {
-	int a, b, l = 0;
-	do
+	int m;
+	for (int t = 0; t < 2; t++)
 	{
-		a = rand() % 9;
-		b = rand() % 9;
-		l++;
-		if (qipan[a][b] != 0)continue;
-		if (!ifqi(a, b, m))continue;
-		setf(m);
-		fanqipan[a][b] = m;
-		if (qipan[a + 1][b] == -m)
-			if (!ifqi(a + 1, b, -m))continue;
-		if (qipan[a - 1][b] == -m)
-			if (!ifqi(a - 1, b, -m))continue;
-		if (qipan[a][b + 1] == -m)
-			if (!ifqi(a, b + 1, -m))continue;
-		if (qipan[a][b - 1] == -m)
-			if (!ifqi(a, b - 1, -m))continue;
-		break;
-	} while (l <= 1000);
-	if (l > 200 && l <= 500)
-		cout << "乐： 可以落子的地方越来越少了，这次我想了好久。" << endl;
-	else if (l > 500 && l <= 1000)
-		cout << "乐： 要决出胜负了吗？我感觉我没有多少地方可以下了。" << endl;
-	if (l > 1000)
-	{
-		cout << endl;
-		cout << "乐： 呃，我可以下这里...嘶，不对,这样提走你的子。" << endl;
-		cout << "乐： 我好像没有地方可以落子了。" << endl;
-		cout << "乐： 好吧，你赢了。" << endl;
-		cout << "You win." << endl;
-		ed = 1;
-		return;
+		if (t == 0)m = 1;
+		else m = -1;
+		for (int a = 0; a <= 8; a++)
+			for (int b = 0; b <= 8; b++)
+			{
+				if (qipan[a][b] != 0)
+				{
+					av[t][a][b] = false; continue;
+				}
+				if (!ifqi(a, b, m))
+				{
+					av[t][a][b] = false; continue;
+				}
+				setf(m);
+				fanqipan[a][b] = m;
+				if ((a+1)<=8&&qipan[a + 1][b] == -m)
+					if (!ifqi(a + 1, b, -m)) { av[t][a][b] = false; continue; }
+				if ((a-1)>=0&&qipan[a - 1][b] == -m)
+					if (!ifqi(a - 1, b, -m)) { av[t][a][b] = false; continue; }
+				if ((b+1)<=8&&qipan[a][b + 1] == -m)
+					if (!ifqi(a, b + 1, -m)) { av[t][a][b] = false; continue; }
+				if ((b-1)>=0&&qipan[a][b - 1] == -m)
+					if (!ifqi(a, b - 1, -m)) { av[t][a][b] = false; continue; }
+				av[t][a][b] = true;
+			}
 	}
-	qipan[a][b] = m;
-	p = a;
-	q = b;
-	return;
+	/*for (int t = 0; t < 2; t++)
+	{
+		for (int a = 0; a <= 8; a++)
+		{
+			for (int b = 0; b <= 8; b++)
+				if (av[t][a][b])cout << "1 ";
+				else cout << "0 ";
+			cout << endl;
+		}
+		cout << endl;
+	}*///调试时使用
 }
 void prt()//打印当前棋盘，调试时使用
 {
@@ -147,6 +161,203 @@ void prt()//打印当前棋盘，调试时使用
 		}
 		cout << endl;
 	}
+	/*cout << "       1 2 3 4 5 6 7 8 9" << endl;
+	for (int i = 0; i <= 8; i++)
+	{
+		cout << "第" << i + 1 << "行" << "  ";
+		for (int j = 0; j <= 8; j++)
+		{
+			cout << muchqi(i, j, -1);
+			if (j != 8)
+				cout << ' ';
+		}
+		cout << endl;
+	}*/
+}
+int randomlyset(int m)//随机走棋
+{
+	ifav();
+	int a, b, l = 0,dd;
+	if (m == 1)dd = 0;
+	else dd = 1;
+	do
+	{
+		a = rand() % 9;
+		b = rand() % 9;
+		//cout << a << ' ' << b << endl;
+		l++;
+		if (av[dd][a][b])
+			break;
+	} while (l <= 1000);
+	if (l == 1000)
+		return l;
+	qipan[a][b] = m;
+	p = a;
+	q = b;
+	return l;
+}
+bool hukou(int x, int y, int m)
+{
+	if (!av[-m][x][y]||!av[m][x][y])
+		return false;
+	int qi=0;
+	if (x <= 7 && qipan[x + 1][y] == 0)qi++;
+	if (x >= 1 && qipan[x - 1][y] == 0)qi++;
+	if (y <= 7 && qipan[x][y + 1] == 0)qi++;
+	if (y >= 1 && qipan[x][y - 1] == 0)qi++;
+	if (qi == 1)
+		return true;
+	else
+		return false;
+}
+int xiejiao(int x, int y, int m)
+{
+	int qi = 0;
+	if (x <= 7 && y <= 7 && qipan[x + 1][y + 1] == m)qi++;
+	if (x >= 1 && y <= 7 && qipan[x - 1][y + 1] == m)qi++;
+	if (x >= 1 && y >= 1 && qipan[x - 1][y - 1] == m)qi++;
+	if (x <= 7 && y >= 1 && qipan[x + 1][y - 1] == m)qi++;
+	return qi;
+}
+int setvalue(int x,int y,int m)//估值函数  需要优化！！！！！
+{
+	int t;
+	if (m == 1)t = 0;
+	else t = 1;
+	if (av[t][x][y])
+	{
+		qipan[x][y] = m;
+		ifav();
+		int zi[2]{ 0,0 };
+		int ttt = 1;
+		for (int t = 0; t < 2; t++)
+		{
+			for (int a = 0; a <= 8; a++)
+				for (int b = 0; b <= 8; b++)
+				{
+					if (av[t][a][b] && (!av[(t + 1) % 2][a][b]))
+						zi[t] += 10;
+					if (hukou(a, b, ttt))
+						zi[t] += 8;
+				}
+			ttt = -1;
+		}
+		if (m == 1)m = 0;
+		if (m == -1)m = 1;
+		value[m][x][y] = zi[m] - zi[(m + 1) % 2]+xiejiao(x,y,m)+xiejiao(x,y,-m);
+		value[-m][x][y] = -value[m][x][y];
+		qipan[x][y] = 0;
+		ifav();
+		return value[m][x][y];
+	}
+	else
+		return -10000;
+}
+/*
+估值函数设计思路：
+    仅自己能下的点：（价值最高）
+	对方不能下的点：价值？搏命时考虑
+	虎口：价值次之，判定较难
+*/
+//void moni()模拟对局，深度优先
+bool ifany(int m)
+{
+	ifav();
+	int jud = 0,op=0;
+	if (m == -1)op = 1;
+	for (int i = 0; i <= 8; i++)
+		for (int j = 0; j <= 8; j++)
+		{
+			if (av[op][i][j])
+			{
+				jud = 1;
+				break;
+			}
+		}
+	if (jud == 0)return false;
+	else return true;
+}
+void set(int m)//所需修改：优先搜索边角，加入价值函数的搜索
+{
+	ifav();
+	int jud = 0;
+	int op,ti=0,val;
+	kexingyu[0][0] = -10000;
+	if (m == -1)op = 1;
+	else op = 0;
+	for (int i = 0; i <= 8; i++)
+		for (int j = 0; j <= 8; j++)
+		{
+			if (av[op][i][j])
+			{
+				jud = 1;
+				break;
+			}
+		}
+	for (int i = 0; i <= 8; i++)
+		for (int j = 0; j <= 8; j++)
+		{
+				val=setvalue(i, j, m);
+				if (val == 0)continue;
+				else
+				{
+					if (val > kexingyu[0][0])
+					{
+						ti = 0;
+						kexingyu[0][0] = val;
+						kexingyu[0][1] = i;
+						kexingyu[0][2] = j;
+						ti++;
+					}
+					else if (val == kexingyu[0][0])
+					{
+						kexingyu[ti][0] = val;
+						kexingyu[ti][1] = i;
+						kexingyu[ti][2] = j;
+						ti++;
+					}
+				}
+		}
+	ifav();
+	int l;
+	if (ti == 0)
+	{
+		if (qipan[0][0] == 0)
+		{
+			p = 0;
+			q = 0;
+			qipan[p][q] = m;
+		}
+		else if (qipan[0][8] == 0)
+		{
+			p = 0;
+			q = 8;
+			qipan[p][q] = m;
+		}
+		else if (qipan[8][0] == 0)
+		{
+			p = 8;
+			q = 0;
+			qipan[p][q] = m;
+		}
+		else if (qipan[8][8] == 0)
+		{
+			p = 8;
+			q = 8;
+			qipan[p][q] = m;
+			kaiju = false;
+		}
+		else
+	      randomlyset(m);
+	}
+	else
+	{
+		l = rand() % ti;
+		p = kexingyu[l][1];
+		q = kexingyu[l][2];
+		qipan[p][q] = m;
+	}
+	return;
 }
 bool checklegal(int l, int r)
 {
@@ -198,6 +409,11 @@ bool huiqi()
 }
 int main()
 {
+	srand((unsigned)time(0));
+	for (int t = 0; t < 2; t++)
+		for (int i = 0; i <= 8; i++)
+			for (int j = 0; j <= 8; j++)
+				av[t][i][j] = true;
 	int k;
 	cout << "      No Go!      " << endl;
 	cout << "  输入0以开始游戏" << endl;
@@ -216,6 +432,7 @@ int main()
 			cout << "  输入0以开始游戏" << endl;
 			cout << "输入1以查看游戏规则" << endl;
 			k = cc();
+			cout << endl;
 		}
 		if (k == 1)
 		{
@@ -283,8 +500,14 @@ int main()
 	while (true)
 	{
 		system("CLS");
-		randomlyset(-k);
-		if (ed)break;
+		set(-k);
+		if (!ifany(k))
+		{
+			cout << endl;
+			prt();
+			cout << "乐：你没有地方可以落子了，你输了！" << endl;
+			break;
+		}
 		cout << endl;
 		cout << "我在第" << p+1 << "行，第" << q+1 << "列落子" << endl;
 		cout << endl;
@@ -302,64 +525,40 @@ int main()
 				continue;
 			}
 			if ((p + 1) <= 8 && qipan[p + 1][q] == -k)
-			{
 				if (!ifqi(p + 1, q, -k))
 				{
-					prt();
-					cout << "乐： 你提走我(" << p + 1 << ',' << q << "）处的子了，你输了" << endl;
-					if (huiqi())
-					{
-						qipan[p][q] = 0;
-						continue;
-					}
-					break;
+					cout << "乐： 你提走我(" << p + 1 << ',' << q << "）处的子了，不可以这样下哦。" << endl;
+					continue;
 				}
-
-			}
 			if ((p - 1) >= 0 && qipan[p - 1][q] == -k)
-			{
 				if (!ifqi(p - 1, q, -k))
 				{
-					prt();
-					cout << "乐： 你提走我(" << p - 1 << ',' << q << "）处的子了，你输了" << endl;
-					if (huiqi())
-					{
-						qipan[p][q] = 0;
-						continue;
-					}
-					break;
+					cout << "乐： 你提走我(" << p + 1 << ',' << q << "）处的子了，不可以这样下哦。" << endl;
+					continue;
 				}
-
-			}
 			if ((q + 1) <= 8 && qipan[p][q + 1] == -k)
-			{
 				if (!ifqi(p, q + 1, -k))
 				{
-					prt();
-					cout << "乐： 你提走我(" << p << ',' << q + 1 << "）处的子了，你输了" << endl;
-					if (huiqi())
-					{
-						qipan[p][q] = 0;
-						continue;
-					}
-					break;
+					cout << "乐： 你提走我(" << p + 1 << ',' << q << "）处的子了，不可以这样下哦。" << endl;
+					continue;
 				}
-
-			}
 			if ((q - 1) >= 0 && qipan[p][q - 1] == -k)
-			{
 				if (!ifqi(p, q - 1, -k))
 				{
-					prt();
-					cout << "乐： 你提走我(" << p << ',' << q - 1 << "）处的子了，你输了" << endl;
-					if (huiqi())
-					{
-						qipan[p][q] = 0;
-						continue;
-					}
-					break;
+					cout << "乐： 你提走我(" << p + 1 << ',' << q << "）处的子了，不可以这样下哦。" << endl;
+					continue;
 				}
-			}
+			break;
+		}
+		/*set(k);
+		if (ed!=0)break;*///调试用，让ai自己与自己下棋
+		if (!ifany(-k))
+		{
+			cout << endl;
+			prt();
+			cout << "乐：我打算下这里...不对，这样会提走你的子" << endl;
+			cout << "乐：诶，我好像没有地方可以下了！" << endl;
+			cout << "乐：好吧，你赢了。" << endl;
 			break;
 		}
 	}
